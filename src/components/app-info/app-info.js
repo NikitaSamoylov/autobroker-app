5087dd6df6fa4515d4e08f34565782c0db9761ed
NikitaSamoylov <nsam7655@gmail.com>
2023-11-04 16:12:51 +0500

    feat: add app header

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,14 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <div className="app-info__content">
+                <h1 className="app-title">учёт автомобилей в AutoBroker</h1>
+                <h2 className="app-subtitle">всего автомобилей: </h2>
+            </div>
+        </div>
+    )
+}
+
+export default AppInfo;
