a5920d85d65a77a3bf3e67761a812b78532463bc
NikitaSamoylov <nsam7655@gmail.com>
2023-11-04 16:39:17 +0500

    feat: add app search

---
+++ src/components/app-search/app-search.js
@@ -0,0 +1,12 @@
+import './app-search.css';
+
+const AppSearch = () => {
+    return (
+        <input type="text"
+                    class="app-search__input"
+                    placeholder="поиск авто ..."
+                    aria-label="поиск авто" />
+    )
+}
+
+export default AppSearch;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,14 @@
 import './app.css';
 import AppInfo from '../app-info/app-info';
+import AppSearch from '../app-search/app-search';
 
 function App() {
     return (
         <div className="app">
             <AppInfo/>
+            <div className="app-search">
+                <AppSearch/>
+            </div>
         </div>
     )
 }
