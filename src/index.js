4993dec759f961b197fdcfaba4d03d3fc517e349
NikitaSamoylov <nsam7655@gmail.com>
2023-11-14 17:34:28 +0300

    feat: add global styles into index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {createGlobalStyle} from 'styled-components';
+// import './index.css';
 import App from './components/app/app';
+
+const Global = createGlobalStyle`
+  body {
+    margin: 0 auto;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+
+  li {
+    list-style-type: none;
+  }
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Global/>
     <App />
   </React.StrictMode>
 );
