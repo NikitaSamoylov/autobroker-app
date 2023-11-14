59fb4c00cec982d6f5b488219029706532a67778
NikitaSamoylov <nsam7655@gmail.com>
2023-11-14 16:00:36 +0300

    feat: turn StyledAppInput into styled component

--- src/components/app-search/app-search.js
+++ src/components/app-search/app-search.js
@@ -1,5 +1,20 @@
 import { Component } from 'react';
-import './app-search.css';
+import styled from 'styled-components';
+// import './app-search.css';
+
+const StyledAppInput = styled.input`
+    display: block;
+    width: 100%;
+    padding: 10px 13px 10px 13px;
+    border-radius: 5px;
+    border: none;
+    margin-bottom: 18px;
+    outline: none;
+    &::placeholder {
+        font-size: 16px;
+        color: rgb(196, 192, 192);
+    }
+`
 
 class AppSearch extends Component {
 
@@ -9,8 +24,7 @@ class AppSearch extends Component {
 
     render() {
         return (
-            <input type="text"
-                    className="app-search__input"
+            <StyledAppInput type="text"
                     placeholder="поиск авто ..."
                     aria-label="поиск авто"
                     value={this.props.searchValue}
